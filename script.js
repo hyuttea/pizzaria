b9a6c6b842ae46271d1df37f8cf5556ca93ce1a1
RafaelGibertoni <rafa08gibertoni@gmail.com>
2025-05-13 17:45:53 -0300

    carrossel att

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-let carosseis = document.getElementsByClassName('carossel-Container')
+let carrosseis = document.getElementsByClassName('carossel-Container');
 
-      for (let i = 0; i < carosseis.length; i++) {
-        let carossel = carosseis[i]
-        let btnBack = carossel.getElementsByClassName('BtnBack')[0]
-        let btnNext = carossel.getElementsByClassName('BtnNext')[0]
+for (let i = 0; i < carrosseis.length; i++) {
+  let carossel = carrosseis[i];
+  let btnBack = carossel.getElementsByClassName('BtnBack')[0];
+  let btnNext = carossel.getElementsByClassName('BtnNext')[0];
 
-        let itens = carossel.getElementsByClassName('item')
-        let posicaoAnterior = 0
-        let mover = posicaoAnterior + 100
+  let itens = carossel.getElementsByClassName('item');
+  let posicaoAtual = 0;
+  let itensPorSessao = 4;
+  let larguraItem = 100; 
+  let maxItemIndex = 9; 
+  let totalItens = itens.length;
 
-        btnNext.addEventListener('click', () => {
-          mover = posicaoAnterior + 100
+  let maxSessoes = Math.floor((maxItemIndex + 1) / itensPorSessao);
 
-          for (let c = 0; c < itens.length; c++) {
-
-            itens[c].style.right = mover + '%'
-
-            posicaoAnterior = mover
-          }
-        })
-
-        btnBack.addEventListener('click', () => {
-          mover = posicaoAnterior - 100
-
-          for (let c = 0; c < itens.length; c++) {
-
-            itens[c].style.right = mover + '%'
-
-            posicaoAnterior = mover
-          }
-
-        })
+  btnNext.addEventListener('click', () => {
+    if (posicaoAtual < maxSessoes) {
+      posicaoAtual++;
+      let deslocamento = posicaoAtual * larguraItem;
+      for (let c = 0; c < itens.length; c++) {
+        itens[c].style.right = deslocamento + '%';
       }
+    }
+  });
+
+  btnBack.addEventListener('click', () => {
+    if (posicaoAtual > 0) {
+      posicaoAtual--;
+      let deslocamento = posicaoAtual * larguraItem;
+      for (let c = 0; c < itens.length; c++) {
+        itens[c].style.right = deslocamento + '%';
+      }
+    }
+  });
+}
